2cb34c0364aec7ef90395096bf91ef65baa0bc54
tctlrd <accts@xsxs.in>
2026-03-10 14:01:34 -0500

    fix tab issue

--- setup-zfsbootmenu.sh
+++ setup-zfsbootmenu.sh
@@ -262,7 +262,7 @@ enter_chroot() {
 	echo "[[LOG]] Installing kernel and ZFS packages..."
 	apt install -y linux-headers-amd64 linux-image-amd64 zfs-initramfs
 	echo "REMAKE_INITRD=yes" > /etc/dkms/zfs.conf
-  echo "UMASK=0077" > /etc/initramfs-tools/conf.d/umask.conf
+	echo "UMASK=0077" > /etc/initramfs-tools/conf.d/umask.conf
 
 	# Set root password
 	echo "[[LOG]] Setting root password."
